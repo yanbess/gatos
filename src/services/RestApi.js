dbead22f02b32e1238855d4bcb3acafee0f8c83d
artem <yanbess18@gmail.com>
2022-10-07 01:23:28 +0200

    clean

--- src/services/RestApi.js
+++ src/services/RestApi.js
@@ -15,7 +15,7 @@ export default class RestApi {
         'Content-Type': 'application/json'
       }
     }
-    //console.log(options)
+
     return axios(options)
       .then((response) => ({ response: response.data, isSuccess: true }))
       .catch((error) => ({ response: error.response, isSuccess: false }))
@@ -29,46 +29,3 @@ export default class RestApi {
     return this.request('post', path, rest.params, data, ...rest)
   }
 }
-
-/* function request(method, path = '', params = {}, data = {}, auth = true) {
-  let options = {
-    url: import.meta.env.VITE_API_URL + path,
-    params,
-    data,
-    method,
-    //timeout: 1000,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  } */
-
-  /* if (auth) {
-    const localStorage = window.localStorage
-    options.headers.Authorization =
-      localStorage.getItem('_auth_type') + ' ' + localStorage.getItem('_auth')
-  } */
-
-/*   return axios(options)
-    .then((response) => ({ response: response.data, isSuccess: true }))
-    .catch((error) => ({ response: error.response, isSuccess: false }))
-} */
-
-/* function get(...rest) {
-  return request('get', ...rest)
-}
-
-function post(path, data = {}, ...rest) {
-  return request('post', path, rest.params, data, ...rest)
-}
- */
-/* const Cats = {
-  list: {
-    get: (limit) => get('images/search', { limit }),
-    /* getDataToCreate: () => get('schedule/data-to-create'),
-    store: (data) => post('schedule/store', data),
-    getEmployees: (data) => post('schedule/get-employees', data),
-    delete: (id) => get('schedule/delete', { id }), */
-  /* }
-}
- */
-/* export default Cats */
